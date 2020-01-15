3609cc53a7cb6fac4aa744572270897fa70365f9
Ben Hernandez <benjammin234@gmail.com>
2020-01-15 15:06:06 -0800

    Adds the date to page

--- src/state/TheDate.js
+++ src/state/TheDate.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 export class TheDate extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { datetime: new Date() }
+    }
+
     render() {
         return(
-            <div />
+            <div>
+                {this.state.datetime.toLocaleString()}
+            </div>
         )
     }
 }
