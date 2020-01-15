241ba33761c4ce181c8bdeb46f5061907c1b67cf
Ben Hernandez <benjammin234@gmail.com>
2020-01-15 14:47:59 -0800

    Updates the playground with experiements

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Split from './composition/Split';
+import './App.css'
+import Tooltip from './composition/Tooltip'
+import Messages from './Messages';
+
+const firstTooltip = (
+  <Tooltip color='hotpink' message='tooltip message'>
+    content
+  </Tooltip>
+)
+
+const secondTooltip = (
+  <Tooltip color='#126BCC' message='another tooltip message'>
+    right
+  </Tooltip>
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className='App'>
+      <Split className="left" flexBasis={2}>
+        Well, this is the {firstTooltip} for the left split. There must be more to this...
+        <Tooltip message='one more tooltip message'>
+          Aurelius?
+        </Tooltip>
+      </Split>
+
+      <Split className="right" flexBasis={3}>
+        This is the content for the {secondTooltip} split. I bet we find out soon...
+      </Split>
+      
+      <div className='App'>
+        <h1>WHAT IS HAPPENING</h1>
+        <Messages name="Messages" unread={0}/>
+        <Messages name="Notifications" unread={10}/>
+      </div>
+    </main>
+  )
 }
 
 export default App;
