2c4f34eedaccf1d7524eeb7a3860ccde724f7247
Ben Hernandez <benjammin234@gmail.com>
2020-01-20 19:03:26 -0800

    Adds Accordion and css

--- src/state-drills/Accordion.js
+++ src/state-drills/Accordion.js
@@ -1,15 +1,41 @@
 import React from 'react';
+import './Accordion.css';
 
 
 
 class Accordion extends React.Component {
-    render() {
+    static defaultProps = {
+        sections: []
+    };
+
+    state = {
+        activeSectionIndex: null
+    }
+
+    handleActiveSection = (sectionIndex) => {
+        this.setState({activeSectionIndex: sectionIndex})
+    }
+
+    renderItem(section, index, activeSectionIndex) {
         return (
-            <div>
-                <ul>
-                    <li></li>
-                </ul>
-            </div>
+            <li className='Accordion_item' key={index}>
+                <button type='button' onClick={() => this.handleActiveSection(index)}>
+                    {section.title}
+                </button>
+                {(activeSectionIndex === index) && <p>{section.content}</p>}
+            </li>
+        )
+    }
+
+    render() {
+        const { activeSectionIndex } = this.state
+        const { sections } = this.props
+        return (
+            <ul className='Accordion'>
+                {sections.map((section, index) => 
+                this.renderItem(section, index, activeSectionIndex)
+                )}
+            </ul>
         )
     }
 }
--- src/state-drills/Accordion.test.js
+++ src/state-drills/Accordion.test.js
@@ -4,7 +4,7 @@ import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import Accordion from './Accordion';
 
-descibe('Accordion component', () => {
+describe('Accordion component', () => {
     const sectionsProps = [
         { title: 'Ice Cream',
             content: 'There is an almost infinite variety of ice cream flavors, but there are only really a handful of flavors that matter. King among those select few is Chocolate Chip Cookie Dough.'},
@@ -18,6 +18,29 @@ descibe('Accordion component', () => {
         const div = document.createElement('div');
         ReactDOM.render(<Accordion />, div);
         ReactDOM.unmountComponentAtNode(div);
-    })    
+    })
+
+    it('renders an empty li when the sections prop is not supplied', () => {
+        const wrapper = shallow(<Accordion />)
+        expect(toJson(wrapper)).toMatchSnapshot()
+    })
+
+    it('renders no section as active by default', () => {
+        const wrapper = shallow(<Accordion sections={sectionsProps} />)
+        expect(toJson(wrapper)).toMatchSnapshot()
+    })
+
+    it('renders a clicked section', () => {
+        const wrapper = shallow(<Accordion sections={sectionsProps} />)
+        wrapper.find('button').at(1).simulate('click')
+        expect(toJson(wrapper)).toMatchSnapshot()
+    })
+
+    it('renders only the last section when multiple sections have been clicked', () => {
+        const wrapper = shallow(<Accordion sections={sectionsProps} />)
+        wrapper.find('button').at(1).simulate('click')
+        wrapper.find('button').at(2).simulate('click')
+        expect(toJson(wrapper)).toMatchSnapshot()
+    })
 
 })
