3125ddc92bfe4cedcce283cdda3fddc645b4bf6f
Ben Hernandez <benjammin234@gmail.com>
2020-02-04 17:21:35 -0800

    Adds name ref and defaultValue

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
-import './index.css';
-import App from './lang-context/AppLang';
+import React from "react";
+import ReactDOM from "react-dom";
+// import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+// import App from "./lang-context/AppLang";
+import RegistrationForm from "./registrationForm/registrationForm";
 
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.getElementById('root')
-);
+// ReactDOM.render(
+//     <BrowserRouter>
+//         <App />
+//     </BrowserRouter>,
+//     document.getElementById('root')
+// );
 
+ReactDOM.render(<RegistrationForm />, document.getElementById("root"));
--- src/registrationForm/registrationForm.js
+++ src/registrationForm/registrationForm.js
@@ -1,9 +1,24 @@
 import React from "react";
 
 export default class RegistrationForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.nameInput = React.createRef();
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const name = this.nameInput.current.value;
+    const password = e.target.password.value;
+    const secondPassword = e.target.repeatPassword.value;
+    console.log(name);
+    console.log(password);
+    console.log(secondPassword);
+  }
+
   render() {
     return (
-      <form className="registration">
+      <form className="registration" onSubmit={e => this.handleSubmit(e)}>
         <h2>Register</h2>
         <div className="registration__hint">* required field</div>
         <div className="form-group">
@@ -13,6 +28,8 @@ export default class RegistrationForm extends React.Component {
             className="registration__control"
             name="name"
             id="name"
+            ref={this.nameInput}
+            defaultValue="Mack"
           />
         </div>
         <div className="form-group">
@@ -23,6 +40,27 @@ export default class RegistrationForm extends React.Component {
             name="password"
             id="password"
           />
+          <div className="registration__hint">
+            6 to 72 characters, must include a number
+          </div>
+        </div>
+        <div className="form-group">
+          <label htmlFor="repeatPassword">Repeat Password *</label>
+          <input
+            type="password"
+            className="registration__control"
+            name="repeatPassword"
+            id="repeatPassword"
+          />
+        </div>
+
+        <div className="registration__button__group">
+          <button type="reset" className="registration__button">
+            Cancel
+          </button>
+          <button type="submit" className="registration__button">
+            Save
+          </button>
         </div>
       </form>
     );
