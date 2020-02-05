896cc3f54acf12c2a891422c2ba6e74d510573e4
Ben Hernandez <benjammin234@gmail.com>
2020-02-04 17:43:44 -0800

    Creates validation functions

--- src/registrationForm/registrationForm.js
+++ src/registrationForm/registrationForm.js
@@ -30,12 +30,38 @@ export default class RegistrationForm extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    const name = this.nameInput.current.value;
-    const password = e.target.password.value;
-    const secondPassword = e.target.repeatPassword.value;
+    const { name, password, repeatPassword } = this.state;
     console.log(name);
     console.log(password);
-    console.log(secondPassword);
+    console.log(repeatPassword);
+  }
+
+  validateName(fieldValue) {
+    const name = this.state.name.value.trim();
+    if (name.length === 0) {
+      return "Name is required";
+    } else if (name.length < 3) {
+      return "Name must be at least 3 characters long";
+    }
+  }
+
+  validatePassword() {
+    const password = this.state.password.value.trim();
+    if (password.length === 0) {
+      return "Password is required";
+    } else if (password.length < 6 || password.length > 72) {
+      return "Password must be between 6 and 72 characters long";
+    } else if (!password.match(/[0-9]/)) {
+      return "Password must contain at least one number";
+    }
+  }
+
+  validateRepeatPassword() {
+    const repeatPassword = this.state.repeatPassword.value.trim();
+    const password = this.state.password.value.trim();
+    if (repeatPassword !== password) {
+      return "Passwords do not match";
+    }
   }
 
   render() {
@@ -50,8 +76,7 @@ export default class RegistrationForm extends React.Component {
             className="registration__control"
             name="name"
             id="name"
-            ref={this.nameInput}
-            defaultValue="Mack"
+            onChange={e => this.updateName(e.target.value)}
           />
         </div>
         <div className="form-group">
@@ -61,6 +86,7 @@ export default class RegistrationForm extends React.Component {
             className="registration__control"
             name="password"
             id="password"
+            onChange={e => this.updatePassword(e.target.value)}
           />
           <div className="registration__hint">
             6 to 72 characters, must include a number
@@ -73,6 +99,7 @@ export default class RegistrationForm extends React.Component {
             className="registration__control"
             name="repeatPassword"
             id="repeatPassword"
+            onChange={e => this.updateRepeatPassword(e.target.value)}
           />
         </div>
 
