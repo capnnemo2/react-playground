35600ccf787006c17f1b1757c566173351bce0e8
Ben Hernandez <benjammin234@gmail.com>
2020-02-04 17:09:22 -0800

    Begins to create form

---
+++ src/registrationForm/registrationForm.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default class RegistrationForm extends React.Component {
+  render() {
+    return (
+      <form className="registration">
+        <h2>Register</h2>
+        <div className="registration__hint">* required field</div>
+        <div className="form-group">
+          <label htmlFor="name">Name *</label>
+          <input
+            type="text"
+            className="registration__control"
+            name="name"
+            id="name"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password *</label>
+          <input
+            type="password"
+            className="registration__control"
+            name="password"
+            id="password"
+          />
+        </div>
+      </form>
+    );
+  }
+}
